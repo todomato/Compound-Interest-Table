4cfb1453fbc33764119f5c5e49dc3827e2085338
todomato <messboy000@gmail.com>
2026-01-07 09:25:19 +0800

    Fix blank screen and setup GitHub Pages config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Compound-Interest-Table/',
       server: {
         port: 3000,
         host: '0.0.0.0',
